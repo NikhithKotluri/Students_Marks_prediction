4767db0d2ba0139a9207286da0ccd54d3930416a
NikhithKotluri <93933573+NikhithKotluri@users.noreply.github.com>
2023-05-02 21:48:14 +0530

    Add files via upload

---
+++ StudentPerformancePrediction-ML-main/tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+gra = input("Grade ID as (G-<grade>): ")
+    if(gra == "G-02"):
+        gra = 2
+    elif (gra == "G-04"):
+        gra = 4
+    elif (gra == "G-05"):
+        gra = 5
+    elif (gra == "G-06"):
+        gra = 6
+    elif (gra == "G-07"):
+        gra = 7
+    elif (gra == "G-08"):
+        gra = 8
+    elif (gra == "G-09"):
+        gra = 9
+    elif (gra == "G-10"):
+        gra = 10
+    elif (gra == "G-11"):
+        gra = 11
+    elif (gra == "G-12"):
+        gra = 12
